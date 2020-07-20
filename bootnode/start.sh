10ae72bdf06977483fe2b36c182963d2675a34e1
chy2315 <chy2315@cht.com.tw>
2020-07-20 13:24:22 +0800

    fix

--- bootnode/start.sh
+++ bootnode/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp -a /root/keystore/. /root/.ethereum/devchain/keystore/
+
 DATADIR="/root/.ethereum/devchain/geth"
 if [ ! -d "${DATADIR}" ]
 then
-	geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
+	geth --datadir=~/.ethereum/devchain init "/root/genesis.json"
 fi
-
+cp -a /root/keystore/. /root/.ethereum/devchain/keystore/
 sleep 3
-geth --nodekey=/root/boot.key ${@}
+geth --nodekey=/root/boot.key --datadir=~/.ethereum/devchain --cache 2048 --lightkdf --gcmode archive --allow-insecure-unlock --rpcapi "db,personal,eth,net,web3,admin" --rpccorsdomain="*" --networkid=54766 --rpc --rpcvhosts="*" --rpcaddr="0.0.0.0" --nodiscover --unlock 0 --password /root/password/password --mine
--- geth-client/start.sh
+++ geth-client/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp -a /root/keystore/. /root/.ethereum/devchain/keystore/
+
 DATADIR="/root/.ethereum/devchain/geth"
 if [ ! -d "${DATADIR}" ]
 then
-	geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
+	geth --datadir=~/.ethereum/devchain init "/root/genesis.json"
 fi
+cp -a /root/keystore/. /root/.ethereum/devchain/keystore/
 sleep 3
-BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
-GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
-geth $GETH_OPTS
+geth --datadir=~/.ethereum/devchain --allow-insecure-unlock --rpccorsdomain="*" --networkid=54766 --rpc --bootnodes="enode://288b97262895b1c7ec61cf314c2e2004407d0a5dc77566877aad1f2a36659c8b698f4b56fd06c4a0c0bf007b4cfb3e7122d907da3b005fa90e724441902eb19e@bootstrap:30303"
+
